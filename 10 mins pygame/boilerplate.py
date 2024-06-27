dce19af4f9d3aee86d88ec5cac851f48905601ce
Ninedeadeyes <tommykwong_84@hotmail.com>
2024-06-27 23:37:44 +0100

    Add files via upload

---
+++ 10 mins pygame/boilerplate.py
@@ -0,0 +1,24 @@
+import pygame
+
+SCREEN_WIDTH =800
+SCREEN_HEIGHT=600
+
+#window 
+
+screen=pygame.display.set_mode(SCREEN_WIDTH,SCREEN_HEIGHT)
+
+
+#game loop 
+
+run=True
+
+while run:
+    #event handler
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+
+
+
+pygame.quit()
+
